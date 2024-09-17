516a8668c4daa4fe0048e36484edd71e85abea59
Tej749 <jargha261975@gmail.com>
2024-09-17 22:42:11 +0545

    navbar & base templates

--- npjAirport/urls.py
+++ npjAirport/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index", views.index),
+    path("", views.index, name="index"),
+    path("form", views.form, name="form"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
 
 ]
--- npjAirport/views.py
+++ npjAirport/views.py
@@ -6,3 +6,15 @@ from .models import Csit
 def index(request):
     data = Csit.objects.all()
     return render(request, "npjAirport/index.html", {'data' : data})
+
+def form(request):
+    return render(request, "npjAirport/form.html")
+
+def about(request):
+    return render(request, "npjAirport/about.html")
+
+def contact(request):
+    return render(request, "npjAirport/contact.html")
+
+
+
