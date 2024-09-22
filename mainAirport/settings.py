6fcc19d8cd7a6d4e4bc3437b2e8101a3135c27c7
Tej749 <jargha261975@gmail.com>
2024-09-22 22:36:01 +0545

    postgresql db connection

--- mainAirport/settings.py
+++ mainAirport/settings.py
@@ -74,12 +74,19 @@ WSGI_APPLICATION = "mainAirport.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'csitairport',
+        'USER': 'postgres',
+        'PASSWORD': 'logic',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
